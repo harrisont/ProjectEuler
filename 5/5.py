b74781bd1e65481c111e49407cbb2bc73c8a655f
Harrison Ting <runhappy@gmail.com>
2012-12-02 15:55:44 -0800

    Solved problem 5.

---
+++ 5/5.py
@@ -0,0 +1,33 @@
+if __name__ == "__main__":
+	import sys
+	sys.path.insert(0, '..')
+from Common import Prime
+
+from collections import Counter
+
+def product(list):
+	p = 1
+	for n in list:
+		p *= n
+	return p
+
+def least_common_multiple_v1(maxDivisor):
+	"""
+	>>> least_common_multiple_v1(10)
+	2520
+	>>> least_common_multiple_v1(20)
+	232792560
+	"""
+	primeFactorsCounter = Counter()
+	for divisor in range(1, maxDivisor+1):
+		# Get the union of the currently-used prime factors and the prime factors of divisor.
+		primeFactorsCounter |= Counter(Prime.prime_factors(divisor))
+	return product(primeFactorsCounter.elements())
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
+
+	maxDivisor = 20
+	print("The smallest positive number that is evenly divisible by all of the numbers from 1 to {:,} (least common multiple) is:".format(maxDivisor))
+	print("Method 1: {}".format(least_common_multiple_v1(maxDivisor)))
